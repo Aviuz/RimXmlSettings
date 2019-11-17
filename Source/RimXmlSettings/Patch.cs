5320d7c09f21f487b8bda9a9b74fd4464df65b49
Avius <avius.ravis@gmail.com>
2019-11-17 15:52:47 +0100

    iteration

---
+++ Source/RimXmlSettings/Patch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using Verse;
+
+namespace RimXmlSettings
+{
+    public class SettingsPatch : PatchOperation
+    {
+        private PatchOperation enabled;
+
+        private PatchOperation disabled;
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            if (SettingsUtility.GetToggleValue("test"))
+            {
+                if (this.enabled != null)
+                {
+                    return this.enabled.Apply(xml);
+                }
+            }
+            else if (this.disabled != null)
+            {
+                return this.disabled.Apply(xml);
+            }
+            return false;
+        }
+    }
+}
---
+++ Source/RimXmlSettings/SettingsUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RimXmlSettings
+{
+    public static class SettingsUtility
+    {
+        public static bool GetToggleValue(string value)
+        {
+            return true;
+        }
+    }
+}
